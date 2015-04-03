075c09a378d3f07ac8a395cf235ded243b7f1d5c
Alan S. Tennent <tennent4@gmail.com>
2015-04-03 16:04:08 -0700

    More optimizations
    Removed lighting, changed tile movement speed

--- Flood/Assets/Tile.js
+++ Flood/Assets/Tile.js
@@ -1,8 +1,8 @@
 ﻿#pragma strict
 class Tile extends MonoBehaviour {
 
-   public var m_front : GameObject;
-   public var m_back : GameObject;
+   public var m_front : Renderer;
+   public var m_back : Renderer;
 
    public static var colors = [
       Color.red,
@@ -38,29 +38,25 @@ class Tile extends MonoBehaviour {
       setColor((Random.Range(0, 1.0) * numColors), Vector2.zero);
    }
 
-
-   // The "set position" operation is a relatively expensive operation compared to get position,
-   // So we perform a check that will only update position if necessary.
-   private function approachZ(z : float) {
-      if (Mathf.Abs(transform.position.z - z) > .005) {
-         transform.position.z = Mathf.Lerp(transform.position.z, z, .3);
-      }
-   }
-
    function UpdateFlipPosition() {
-
       var elapsedTime = Time.time - flipStartTime;
-      if (elapsedTime > .25) {
-         approachZ(0);
-      } 
-      else if (elapsedTime > 0) { 
-         approachZ(-1);
-      }
+      if (elapsedTime > 0) {
 
-      // "set rotation" requires a similar optimization as approachZ.
-      if (elapsedTime > 0 && 
-            Quaternion.Angle(transform.rotation, desiredRotation) > .1) { 
-         transform.rotation = Quaternion.Lerp(transform.rotation, desiredRotation, elapsedTime*1.5);
+         // The "set position" operation is relatively expensive compared to get position,
+         // So we perform a check that will only update position if necessary.
+         var desiredZ = (elapsedTime > .25) ? 0 : -1;
+         if (Mathf.Abs(transform.position.z - desiredZ) > .001) {
+            transform.position.z = Mathf.Lerp(transform.position.z, desiredZ, elapsedTime*1.2);
+         }
+
+         // "set rotation" benefits from a similar optimization as position.
+         if (m_back.enabled) {
+            if (Quaternion.Angle(transform.rotation, desiredRotation) > .01) { 
+               transform.rotation = Quaternion.Lerp(transform.rotation, desiredRotation, elapsedTime*1.5);
+            } else {
+               m_back.enabled = false;
+            }
+         }
       }
    }
 
@@ -70,25 +66,29 @@ class Tile extends MonoBehaviour {
 
    // Returns the time offset so that the board can avoid delay when clicking far away.
    public function setColor(colorIndex : int, clickOrigin : Vector2) {
-      transform.rotation = desiredRotation;
-      transform.position.z = 0;
+      if (Mathf.Abs(transform.position.z) > .005) {
+         transform.position.z = 0;
+      }
 
       // set up rotation.
+      transform.rotation = desiredRotation;
       transform.Rotate(Vector3.forward, 180);
       desiredRotation = transform.rotation;
       transform.Rotate(Vector3.forward, 180);
 
 
-      m_back.GetComponent.<Renderer>().material.color = colors[colorIndex];
+      m_back.material.color = colors[colorIndex];
 
       var animationDelay = Vector2.Distance(gridPosition, clickOrigin)/30.0;
       flipStartTime = Time.time + animationDelay;
 
-      // Swap
+      // Ensure this is enabled (it probably is not, unless the user is clicking really fast)
+      m_back.enabled = true;
+      
+      // Swap back and front.
       var temp = m_front;
       m_front = m_back;
       m_back = temp;
-
 
       // Don't flip if you're already the right color
       if (m_colorIndex == colorIndex) {
